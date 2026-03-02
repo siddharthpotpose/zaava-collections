0e0bebfce8e813f227399ef15d893108cdb56c80
SiddharthPotpose <siddharthpotpose9@gmail.com>
2026-03-02 10:27:29 +0530

    new changes

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production : false,
     // apiUrl : 'https://freeapi.miniprojectideas.com/api/BigBasket'
-    apiUrl : 'https://corsproxy.io/?https://freeapi.miniprojectideas.com/api/BigBasket'
+    apiUrl : 'https://corsproxy.io/?https://freeapi.miniprojectideas.com/api/BigBasket' 
 
 }
